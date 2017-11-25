b3fb2203e974c6cfabd3bdac7788c8cbcef73805
Xl.W <shawlix+git@gmail.com>
2017-11-25 22:49:50 +0800

    ef51: add MultiHal config for sensor [1/2]
    Change-Id: I808dcbc280f5a637eaa29f93ba0fbee6dddcb040

--- device.mk
+++ device.mk
@@ -60,6 +60,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     Torch
 
+# Sensors
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sensors/_hals.conf:system/vendor/etc/sensors/_hals.conf
+
+# Thermal
 PRODUCT_COPY_FILES += \
 	device/pantech/ef51/thermald/thermald.conf:$(TARGET_OUT_VENDOR)/vendor/etc/thermald.conf \
 	device/pantech/ef51/thermald/thermal-engine-8064.conf:$(TARGET_OUT_VENDOR)/vendor/etc/thermal-engine-8064.conf
