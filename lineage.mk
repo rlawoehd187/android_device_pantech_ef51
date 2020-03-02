eae4980d947890a5aee5b05ea958eed7d6a0daa7
hiru <rlawoehd187@naver.com>
2020-03-02 13:38:41 +0900

    ef51: bringup for O.

--- lineage.mk
+++ lineage.mk
@@ -1,13 +1,25 @@
+#
+# Copyright (C) 2015-2016 The CyanogenMod Project
+#               2017-2018 The LineageOS Projec
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
-# Inherit some common Slim stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
-# Inherit device configuration
-$(call inherit-product, device/pantech/ef51/full_ef51.mk)
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := ef51
@@ -17,5 +29,14 @@ PRODUCT_MODEL := Vega No.6
 PRODUCT_MANUFACTURER := Pantech
 
 TARGET_DEVICE := ef51
+TARGET_VENDOR := Pantech
+TARGET_VENDOR_PRODUCT_NAME := ef51
+TARGET_VENDOR_DEVICE_NAME := ef51
 
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="msm8960-user 4.4.2 KOT49H IM-A860S.103 release-keys" 
 
+BUILD_FINGERPRINT := VEGA/VEGA_IM-A860S/ef51s:4.4.2/KOT49H/IM-A860S.103:user/release-keys
+
+# Inherit device configuration
+$(call inherit-product, device/pantech/ef51/full_ef51.mk)
--- releasetools/device_check.sh
+++ releasetools/device_check.sh
@@ -1,17 +1,17 @@
 #!/sbin/sh
 
 if grep -q "IM-A860S" /dev/block/mmcblk0p12 ; then
-	mv /system/etc/firmware_ef51s/* /system/etc/firmware/
+	mv /system/vendor/etc/firmware_ef51s/* /system/vendor/firmware/
 
 elif grep -q "IM-A860K" /dev/block/mmcblk0p12 ; then
-	mv /system/etc/firmware_ef51k/* /system/etc/firmware/
+	mv /system/vendor/etc/firmware_ef51k/* /system/vendor/firmware/
 
 elif grep -q "IM-A860L" /dev/block/mmcblk0p12 ; then
-	mv /system/etc/firmware_ef51l/* /system/etc/firmware/
+	mv /system/vendor/etc/firmware_ef51l/* /system/vendor/firmware/
 fi
 
-rm -rf /system/etc/firmware_ef51s
-rm -rf /system/etc/firmware_ef51k
-rm -rf /system/etc/firmware_ef51l
-rm -rf /system/bin/device_check.sh
+rm -rf /system/vendor/etc/firmware_ef51s
+rm -rf /system/vendor/etc/firmware_ef51k
+rm -rf /system/vendor/etc/firmware_ef51l
+rm -rf /system/vendor/bin/device_check.sh
 
